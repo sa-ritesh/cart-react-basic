3e0f4e8eff2c3bea80bacfc4b544645e28d0aa86
Ritesh <arora.ritesh.95@gmail.com>
2021-11-04 17:10:15 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Cart from "./Cart";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <React.Fragment>
+      <Navbar />
       <Cart />
     </React.Fragment>
   );
